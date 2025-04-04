8c9e59efbf4193c432db8c32ca16571eafbb5f6e
m-xim <170838360+m-xim@users.noreply.github.com>
2025-04-04 07:04:05 +0300

    fix: path config

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from dynaconf import Dynaconf
 from pydantic import BaseModel
 
 from src.logs.config import LogConfig
+from src.utils.resource_path import resource_path
 
 
 class Main(BaseModel):
@@ -18,7 +19,7 @@ class Config(BaseModel):
 
 
 def parse_config():
-    settings = Dynaconf(envvar_prefix=False, settings_files=["settings.toml"], load_dotenv=False)
+    settings = Dynaconf(envvar_prefix=False, settings_files=[resource_path("settings.toml")], load_dotenv=False)
     return Config.model_validate(settings.as_dict())
 
 
